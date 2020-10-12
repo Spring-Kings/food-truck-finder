7db79cc5123c4234975de9b2a8966321f797ebc9
Kevin Demars <kevin_demars1@baylor.edu>
2020-10-12 17:00:41 -0500

    Added logo placeholder and POC navbar
    Signed-off-by: Kevin Demars <kevin_demars1@baylor.edu>

--- food-truck-frontend/src/components/SiteHeader.tsx
+++ food-truck-frontend/src/components/SiteHeader.tsx
@@ -1,7 +1,7 @@
 import styles from './layout.module.css'
 import React, {Component} from 'react'
-import {Grid} from '@material-ui/core'
-import {siteTitle} from "./CoolLayout";
+import {Breadcrumbs, Grid, Link as MuiLink} from '@material-ui/core'
+import NextLink from 'next/link'
 
 type State = {}
 
@@ -10,8 +10,29 @@ type Props = {}
 class SiteHeader extends Component<Props, State> {
     render() {
         return (
-            <Grid container className={styles.header}>
-                {siteTitle}
+            <Grid container className={styles.header} direction="column" alignItems="center" spacing={1}>
+                <Grid item>
+                    <NextLink href="/" passHref>
+                        <img src="/logo.png" alt="logo" width="128px"/>
+                    </NextLink>
+                </Grid>
+                <Grid item>
+                    <Breadcrumbs>
+                        <NextLink href="/" passHref>
+                            <MuiLink>Home</MuiLink>
+                        </NextLink>
+                        <NextLink href="/register" passHref>
+                            <MuiLink>Register</MuiLink>
+                        </NextLink>
+                        <NextLink href="/login" passHref>
+                            <MuiLink>Login</MuiLink>
+                        </NextLink>
+                        <NextLink href="/" passHref>
+                            <MuiLink>etc.</MuiLink>
+                        </NextLink>
+                    </Breadcrumbs>
+                </Grid>
+
             </Grid>
         )
     }
