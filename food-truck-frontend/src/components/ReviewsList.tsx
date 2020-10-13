b7bf3ea39b48db891c66980f9a727cf4183e8158
Micah <micah_schiewe1@baylor.edu>
2020-10-12 20:08:57 -0500

    Revert "Merge branch 'master' of https://github.com/Spring-Kings/food-truck-finder"
    This reverts commit 2cd7d5b40cbced932cc37f47c62a5606cc46c4ce, reversing
    changes made to d3b6ac8e201bd6525a7bdaa9804ace7e169d24ff.

--- food-truck-frontend/src/components/ReviewsList.tsx
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-
-class ReviewsList extends React.Component{
-    render() {
-        return (<h1>ReviewList</h1>);
-    }
-}
--- food-truck-frontend/src/components/SubscriptionList.tsx
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-
-class SubscriptionList extends React.Component{
-    render() {
-        return (<h1>Subscription List</h1>);
-    }
-}
