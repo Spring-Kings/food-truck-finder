925bca4ae0144b2226567f7a72e8f8d47c3a1d10
Micah <micah_schiewe1@baylor.edu>
2020-10-14 18:56:09 -0500

    Copy basic scaffolding from my branch
    * It'll diverge enough that it makes sense to have two separate maps.
      Just getting boilerplate out of the way.

--- food-truck-frontend/src/pages/routes.js
+++ food-truck-frontend/src/pages/routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import RouteMapComponent from "../components/map/route-map/RouteMap";
 
 function RoutesPage(){
     return(
         <div>
             <p>Routes Page</p>
+            <RouteMapComponent />
         </div>
     )
 }
