5778ff99015e006ffbd83079fea028ea25459019
nnazo <jacobcurtis2266@gmail.com>
2020-11-04 20:30:41 -0600

    add reducer for notifications

--- food-truck-frontend/src/util/redux.js
+++ food-truck-frontend/src/util/redux.js
@@ -5,8 +5,12 @@ import thunk from 'redux-thunk';
 import userReducer from "../redux/user/UserReducer";
 import { userReduxName } from "../redux/user/UserActions";
 
+import notificationsReducer from "../redux/notifications/NotificationReducer";
+import { notificationsReduxName } from "../redux/notifications/NotificationActions";
+
 const reducers = combineReducers({
-    [userReduxName]: userReducer.reducer
+    [userReduxName]: userReducer.reducer,
+    [notificationsReduxName]: notificationsReducer.reducer
 });
 
 export const buildStore = (initialState) => {
