aa8d4789a00fff507cf374cb2c9c97513b445576
nnazo <jacobcurtis2266@gmail.com>
2020-11-04 19:56:02 -0600

    begin converting notifications to redux

---
+++ food-truck-frontend/src/components/notifications/Notifications.ts
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+
+import NotificationListComponent from "./NotificationListComponent";
+import mapDispatchToProps from "./NotificationD2P";
+
+function mapStateToProps(state: any, myProps: any) {
+  return state.notifications.data;
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NotificationListComponent);
