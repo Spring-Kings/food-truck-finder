9cbb405f432775c4d484218a84837c41767c56fe
nnazo <jacobcurtis2266@gmail.com>
2020-11-25 13:08:38 -0600

    format dates in notifications

--- food-truck-frontend/src/components/notifications/NotificationComponent.tsx
+++ food-truck-frontend/src/components/notifications/NotificationComponent.tsx
@@ -10,18 +10,7 @@ interface NotificationProps {
 function NotificationComponent(props: NotificationProps) {
   const [read, setRead]: [boolean, any] = useState(props.notification.read);
 
-  const modify = <Grid container direction="row">
-    <Grid item>
-      <Button color="secondary"
-              onClick={async () => {
-                props.deletedCallback(props.notification.id);
-                if (props.notification.type === "SUBSCRIPTION") {
-                  await deleteNotification(props.notification.id);
-                }
-              }}>
-        Delete
-      </Button>
-    </Grid>
+  const modify = <Grid container direction="row" justify="flex-end" alignItems="flex-end">
     <Grid item>
       {"Read"}
       <Switch value={read}
@@ -33,21 +22,54 @@ function NotificationComponent(props: NotificationProps) {
               name="Read"
       />
     </Grid>
+    <Grid item>
+      <Button color="secondary"
+              onClick={async () => {
+                props.deletedCallback(props.notification.id);
+                if (props.notification.type === "SUBSCRIPTION") {
+                  await deleteNotification(props.notification.id);
+                }
+              }}>
+        Delete
+      </Button>
+    </Grid>
   </Grid>;
+
+  // Not parsing this causes a RangeError exception in DateTimeFormat.format() because
+  // it claims the date value is not finite
+  //@ts-ignore
+  const parsedDate = Date.parse(props.notification.time);
+
+  const timeOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
+  const date = new Intl.DateTimeFormat('en-US').format(parsedDate);
+  const time = new Intl.DateTimeFormat('en-US', timeOptions).format(parsedDate);
 
   return (
     <Grid item xs>
       <Card>
         <CardContent>
-          <Typography variant="h6">
-            {`From: ${props.notification.truck.name} at ${props.notification.time.toLocaleString('en-US')}`}
-          </Typography>
+          <Grid container direction="row">
+            <Grid item>
+              <Typography variant="h6">
+                {`From: ${props.notification.truck.name}`}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h6">
+                {`${date} ${time}`}
+              </Typography>
+            </Grid>
+          </Grid>
+          
           <Typography>
             {props.notification.message}
           </Typography>
         </CardContent>
         <CardActions>
-
           {props.notification.type === "SUBSCRIPTION" ? modify : " (Nearby Notification)"}
         </CardActions>
       </Card>
