562ae789f5dc7e57c95e88045a33d158bf57e1cc
Micah <micah_schiewe1@baylor.edu>
2020-11-27 13:34:18 -0600

    Dropped broken start script

--- food-truck-semantic/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ./venv/Scripts/activate
-
-# Gotten from: https://spacy.io/models/en-starters#en_vectors_web_lg
-pip install -r ./requirements.txt
-python -m spacy download en_vectors_web_lg
-
-# Start service
-flask run
