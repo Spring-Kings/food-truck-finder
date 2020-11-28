b1104d4a6d2e44ba270f256d0401e47e7444b239
Micah <micah_schiewe1@baylor.edu>
2020-11-27 21:20:48 -0600

    Fix route API test to pass

--- food-truck-frontend/src/api/__tests__/RouteLocation.test.ts
+++ food-truck-frontend/src/api/__tests__/RouteLocation.test.ts
@@ -4,12 +4,20 @@ import mock_route from "./route/mock_backend_response.json";
 import mock_route_locs from "./route/frontend_oracle.json";
 import API_SUITE from "../../util/test_util/api_tests";
 
+// Create oracle
+const today = new Date(Date.now()).toISOString().split("T")[0];
+let mock_result_data = mock_route_locs.map(mrl => ({
+  ...mrl,
+  arrivalTime: `${today}${mrl.arrivalTime}`,
+  exitTime: `${today}${mrl.exitTime}`
+}));
+
 API_SUITE("Test Route Location API", [
   {
     testName: "loadTodaysRoute parses a route properly",
     apiCall: async () => await RouteLocationApi.loadTodaysRoute(1, fail),
     mockResponse: mock_route,
-    actualResponse: mock_route_locs
+    actualResponse: mock_result_data
   },
   {
     testName: "loadTodaysRoute returns empty correctly",
