3d11eb546708a49ab2f895d3fa522a6fb55ed90f
Kevin Demars <kevin_demars1@baylor.edu>
2020-11-30 17:33:19 -0600

    Quick fix for viewing truck as guest
    Signed-off-by: Kevin Demars <kevin_demars1@baylor.edu>

--- food-truck-frontend/src/api/SubscriptionApi.ts
+++ food-truck-frontend/src/api/SubscriptionApi.ts
@@ -3,10 +3,15 @@ import Subscription, {SubscriptionMeta} from "../domain/Subscription";
 import {parse} from "../util/type-checking";
 
 export const getSubscriptionForTruck = async (truckId: number): Promise<Subscription | null> => {
-  let res = await api.get(`/truck/${truckId}/subscription`);
-  if (res.data == null)
+  try {
+    let res = await api.get(`/truck/${truckId}/subscription`);
+    if (res.data == null)
+      return null;
+    return parse(SubscriptionMeta, res.data);
+  } catch (e) {
+    console.log(e);
     return null;
-  return parse(SubscriptionMeta, res.data);
+  }
 }
 
 export const subscribeToTruck = async (truckId: number): Promise<Subscription | null> => {
