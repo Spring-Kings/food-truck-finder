5002764529fb440a8c3c9ea2f17e67cbb74ba1ed
nnazo <jacobcurtis2266@gmail.com>
2020-12-01 13:48:56 -0600

    remove console logs

--- food-truck-frontend/src/domain/RouteLocation.ts
+++ food-truck-frontend/src/domain/RouteLocation.ts
@@ -23,23 +23,18 @@ export type RouteLocation = t.TypeOf<typeof RouteLocationMeta>
 export const wrapsAroundMidnight = (loc: RouteLocation) => secondsWithinDay(loc.exitTime) < secondsWithinDay(loc.arrivalTime);
 
 export const locationsConflict = (loc1: RouteLocation, loc2: RouteLocation) => {
-  console.log("Loc1:", loc1, "Loc2:", loc2)
   const arrival1 = secondsWithinDay(loc1.arrivalTime);
   const exit1 = secondsWithinDay(loc1.exitTime);
   const arrival2 = secondsWithinDay(loc2.arrivalTime);
   const exit2 = secondsWithinDay(loc2.exitTime);
 
   if (!wrapsAroundMidnight(loc1) && !wrapsAroundMidnight(loc2)) {
-    console.log("A")
     return arrival1 <= exit2 && exit1 >= arrival2;
   } else if (wrapsAroundMidnight(loc1) && !wrapsAroundMidnight(loc2)) {
-    console.log("B")
-    return exit1 >= arrival2 || arrival1 <= exit2
+    return exit1 >= arrival2 || arrival1 <= exit2;
   } else if (!wrapsAroundMidnight(loc1) && wrapsAroundMidnight(loc2)) {
-    console.log("C")
-    return arrival1 <= exit2 || exit1 >= arrival2
+    return arrival1 <= exit2 || exit1 >= arrival2;
   } else {
-    console.log("D")
     return true; // If both wrap around, that means they conflict at midnight
   }
 }
